21618f419c9b90cdd2799b5605acfefbc643bf4d
raphson1 <raphsoneric@gmail.com>
2022-06-29 12:48:28 -0500

    categories routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,14 +1,34 @@
 const router = require('express').Router();
+const { Model } = require('sequelize');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{
+        model: Product
+      }]
+    });
+
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{
+        model: Product
+      }]
+    });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
